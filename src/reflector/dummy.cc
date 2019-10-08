ee37635f78ad796bc637c9e81a53d6679c5c3a4b
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-10-08 12:29:24 +0200

    working on member info

---
+++ src/reflector/dummy.cc
@@ -0,0 +1,1 @@
+// left empty for now (to make this not a header-only lib)
---
+++ src/reflector/introspect.hh
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <reflector/helper.hh>
+
+namespace rf
+{
+template <class T>
+static constexpr bool is_introspectable = decltype(rf::detail::is_introspectable_test(std::declval<T&>()))::value;
+
+template <class Inspector, class T>
+constexpr void do_introspect(Inspector&& inspector, T& t)
+{
+    static_assert(is_introspectable<T>, "type is not introspectable.");
+
+    // TODO: check if inspectable, other customization points
+    introspect(inspector, t);
+}
+}
