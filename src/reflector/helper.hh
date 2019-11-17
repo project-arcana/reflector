50006630c58525a568350329ae4801bbc6267bfa
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-11-17 22:02:43 +0100

    Fix MSVC compilation

--- src/reflector/helper.hh
+++ src/reflector/helper.hh
@@ -9,6 +9,6 @@ struct dummy_inspector
 };
 
 template <class T>
-auto is_introspectable_test(T const& t) -> decltype(introspect(dummy_inspector{}, const_cast<T&>(t)), std::true_type{});
+auto is_introspectable_test(T& t) -> decltype(introspect(dummy_inspector{}, t), std::true_type{});
 std::false_type is_introspectable_test(...);
 }
