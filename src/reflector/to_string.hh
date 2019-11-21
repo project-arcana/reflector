6df82fec07555b9de0c9a3d4931ebab07b88d6d4
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-11-21 14:18:43 +0100

    made to_string SFINAE friendly

--- src/reflector/to_string.hh
+++ src/reflector/to_string.hh
@@ -1,12 +1,6 @@
 #pragma once
 
-#include <clean-core/always_false.hh>
-#include <clean-core/priority_tag.hh>
-#include <clean-core/string.hh>
-#include <clean-core/string_view.hh>
-#include <clean-core/to_string.hh>
-
-#include <reflector/introspect.hh>
+#include <reflector/detail/stringify.hh>
 
 namespace rf
 {
@@ -16,79 +10,14 @@ namespace rf
 /// * to_string(value)
 /// * cc::to_string(value)
 /// * introspect(..., value)
-/// * <compile error>
 ///
 /// TODO: implement stream-based version for better performance
 template <class T>
-cc::string to_string(T const& value);
-}
-
-namespace rf_external_detail
+auto to_string(T const& value) -> decltype(::rf_external_detail::impl_to_string(value, cc::priority_tag<2>{}))
 {
-struct stringifier
-{
-    cc::string& s;
-
-    int cnt = 0;
-
-    template <class T>
-    void operator()(T const& v, cc::string_view name)
-    {
-        if (cnt > 0)
-            s += ", ";
-
-        s += name;
-        s += ": ";
-
-        if constexpr (std::is_convertible_v<T, cc::string>)
-        {
-            s += '"';
-            s += v; // TODO: quote?
-            s += '"';
-        }
-        else
-            s += rf::to_string(v);
-
-        ++cnt;
-    }
-
-    stringifier(cc::string& s) : s(s) { s += "{ "; }
-    ~stringifier() { s += " }"; }
-};
-
-template <class T>
-auto impl_to_string(T const& value, cc::priority_tag<3>) -> decltype(to_string(value))
-{
-    return to_string(value);
+    return ::rf_external_detail::impl_to_string(value, cc::priority_tag<2>{});
 }
 
 template <class T>
-auto impl_to_string(T const& value, cc::priority_tag<2>) -> decltype(cc::to_string(value))
-{
-    return cc::to_string(value);
-}
-
-template <class T, class = std::enable_if_t<rf::is_introspectable<T>>>
-cc::string impl_to_string(T const& value, cc::priority_tag<1>)
-{
-    cc::string str;
-    {
-        auto s = stringifier(str);
-        rf::do_introspect(s, const_cast<T&>(value)); // promise we will not change anything!
-    }
-    return str;
-}
-
-template <class T>
-cc::string impl_to_string(T const& value, cc::priority_tag<0>)
-{
-    static_assert(cc::always_false<T>, "unable to stringify the given type");
-    return {};
-}
-}
-
-template <class T>
-cc::string rf::to_string(T const& value)
-{
-    return ::rf_external_detail::impl_to_string(value, cc::priority_tag<3>{});
+static constexpr bool has_to_string = ::rf_external_detail::has_to_string_t<T>::value;
 }
