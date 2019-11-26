f693d5fdb44b1eb214dc87e5d59664a5afeaae6b
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-11-26 22:31:54 +0100

    Add rf::member_compare

---
+++ src/reflector/member_compare.hh
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <clean-core/bit_cast.hh>
+#include <clean-core/string_view.hh>
+
+#include <reflector/introspect.hh>
+
+namespace rf
+{
+namespace detail
+{
+struct MemberwiseComparator
+{
+    std::byte const* lhs_raw;
+    std::byte const* rhs_raw;
+    bool is_equal = true;
+
+    template <class T, class... Args>
+    void operator()(T const& rhs_member, cc::string_view, Args&&...) noexcept
+    {
+        if (is_equal)
+        {
+            size_t const member_offset = cc::bit_cast<std::byte*>(&rhs_member) - rhs_raw;
+            T const& lhs_member = *reinterpret_cast<T const*>(lhs_raw + member_offset);
+            is_equal = is_equal && (rhs_member == lhs_member);
+        }
+    }
+};
+}
+
+template <class T>
+[[nodiscard]] bool member_compare(T const& lhs, T const& rhs) noexcept
+{
+    detail::MemberwiseComparator comparator;
+    comparator.lhs_raw = cc::bit_cast<std::byte const*>(&lhs);
+    comparator.rhs_raw = cc::bit_cast<std::byte const*>(&rhs);
+    do_introspect<T>(comparator, const_cast<T&>(rhs));
+    return comparator.is_equal;
+}
+}
