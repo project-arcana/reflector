f628c014a696d60cc82df9b27a722a3ed8f263af
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-11-27 11:00:46 +0100

    Add bounds assert and static asserts to member compare

--- src/reflector/member_compare.hh
+++ src/reflector/member_compare.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <clean-core/assert.hh>
 #include <clean-core/bit_cast.hh>
 
 #include <reflector/introspect.hh>
@@ -8,6 +9,7 @@ namespace rf
 {
 namespace detail
 {
+template <class OuterT>
 struct MemberwiseComparator
 {
     std::byte const* lhs_raw;
@@ -17,9 +19,11 @@ struct MemberwiseComparator
     template <class T, class... Args>
     void operator()(T const& rhs_member, char const*, Args&&...) noexcept
     {
+        static_assert(sizeof(T) > 0, "No incomplete members allowed");
         if (is_equal)
         {
             size_t const member_offset = cc::bit_cast<std::byte const*>(&rhs_member) - rhs_raw;
+            CC_ASSERT(member_offset < sizeof(OuterT));
             T const& lhs_member = *reinterpret_cast<T const*>(lhs_raw + member_offset);
             is_equal = (rhs_member == lhs_member);
         }
@@ -31,7 +35,8 @@ struct MemberwiseComparator
 template <class T>
 [[nodiscard]] bool member_compare(T const& lhs, T const& rhs) noexcept
 {
-    detail::MemberwiseComparator comparator;
+    static_assert(sizeof(T) > 0, "No incomplete types allowed");
+    detail::MemberwiseComparator<T> comparator;
     comparator.lhs_raw = cc::bit_cast<std::byte const*>(&lhs);
     comparator.rhs_raw = cc::bit_cast<std::byte const*>(&rhs);
     do_introspect<T>(comparator, const_cast<T&>(rhs));
