2e48d756a9ba27ce59360e58dc2d8b4b0285197a
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2020-09-27 08:54:53 +0200

    small tweak to comment

--- src/reflector/introspect.hh
+++ src/reflector/introspect.hh
@@ -8,7 +8,7 @@ template <class T, class Inspector>
 constexpr void do_introspect(Inspector&& inspector, T& t)
 {
     static_assert(!std::is_const_v<std::remove_reference_t<T>>, "do_introspect must be called with an l-value ref");
-    static_assert(is_introspectable<T>, "type is not introspectable. provide `template<class In> constexpr void introspect(In&& insp, T& type);'");
+    static_assert(is_introspectable<T>, "type is not introspectable. provide `template <class In> constexpr void introspect(In&& inspect, T& v);'");
 
     // TODO: other customization points
     introspect(inspector, t);
