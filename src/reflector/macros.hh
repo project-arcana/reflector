cc7875500a263dcafdebe661844236c67728bd89
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-10-08 11:17:52 +0200

    Make macro names shorter

--- src/reflector/macros.hh
+++ src/reflector/macros.hh
@@ -6,15 +6,15 @@
  *
  * usage:
  *
- *  REFL_INTROSPECT_FUNC(MyStructType)
+ *  REFL_INTROSPECT(MyStructType)
  *  {
- *      REFL_INTROSPECT_FIELD(f1);
- *      REFL_INTROSPECT_FIELD(f2);
- *      REFL_INTROSPECT_FIELD(f3);
+ *      REFL_FIELD(f1);
+ *      REFL_FIELD(f2);
+ *      REFL_FIELD(f3);
  *      // ... list all fields ...
  *
  *      // or multiple fields at once
- *      REFL_INTROSPECT_FIELD4(scale, bias, f7, f8);
+ *      REFL_FIELD4(scale, bias, f7, f8);
  *
  *      // custom names
  *      inspect(v.scaleInv, "inverse scale");
@@ -22,14 +22,14 @@
  *
  */
 
-#define REFL_INTROSPECT_FUNC(_type_) \
-    template <class In>              \
+#define REFL_INTROSPECT(_type_) \
+    template <class In>         \
     constexpr void introspect(In&& inspect, _type_& v)
 
-#define REFL_INTROSPECT_FIELD(_field_) inspect(v._field_, #_field_)
+#define REFL_FIELD(_field_) inspect(v._field_, #_field_)
 
-#define REFL_INTROSPECT_FIELD4(_field1_, _field2_, _field3_, _field4_) \
-    REFL_INTROSPECT_FIELD(_field1_);                                   \
-    REFL_INTROSPECT_FIELD(_field2_);                                   \
-    REFL_INTROSPECT_FIELD(_field3_);                                   \
-    REFL_INTROSPECT_FIELD(_field4_)
+#define REFL_FIELD4(_field1_, _field2_, _field3_, _field4_) \
+    REFL_FIELD(_field1_);                                   \
+    REFL_FIELD(_field2_);                                   \
+    REFL_FIELD(_field3_);                                   \
+    REFL_FIELD(_field4_)
